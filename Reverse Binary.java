19b51e6d4960df8c677e284a675262603d088e68
Rakesh0060 <112700200+Rakesh0060@users.noreply.github.com>
2023-02-17 13:10:26 +0530

    Create Reverse Binary.java

---
+++ Reverse Binary.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class revbin
+{
+public static void main(String[] args)
+{
+int b;
+System.out.print("Enter a decimal number=");
+Scanner sc=new Scanner(System.in);
+b=sc.nextInt();
+System.out.print(Integer.toBinaryString(b));
+}
+}
