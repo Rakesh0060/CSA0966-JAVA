e5b1bf92c678b8a68aacaddace5228e4703a62b8
Rakesh0060 <112700200+Rakesh0060@users.noreply.github.com>
2023-02-17 12:59:52 +0530

    Create power of three.java

---
+++ power of three.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class cube
+{
+public static void main(String [] args)
+{
+int a,i=4;
+System.out.print("Enter a number=");
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+if(a==(i*i*i))
+{
+System.out.print("True");
+}
+else if(a!=(i*i*i))
+{
+System.out.print("False");
+}
+}
+}
